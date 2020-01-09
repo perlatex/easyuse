5a9e0a74229ed4ded24b8f6b19c242c8225be04c
perlatex <wangmj@sicnu.edu.cn>
2020-01-09 08:29:09 +0800

    rlang_squash

--- R/cutoffs_modify_at.R
+++ R/cutoffs_modify_at.R
@@ -1,12 +1,12 @@
 #' @title  To obtain deprivation data.frame 
-#' @description To be specific, given a cutoff value for each variables of data.frame, 
+#' @description To be specific, given a cutoff-value for each variables of data.frame, 
 #' if the value of variable is greater than the corresponding cutoff, the value of variable will be changed to 0, otherwise changed to 1.
 #' 
 #' Thanks (Misha Balyasin)[https://github.com/romatik] for great help.
 #'
 #' @param df data.frame
-#' @param .vars specify the variables which to be cutoffs, for example, .vars = c("x", "y", "z")
-#' @param cutoffs a list or vector containts cutoffs value for variables, 
+#' @param .vars specify the variables which to be cutoffs, for example, .vars = c(x, y, z) or .vars = x
+#' @param cutoffs a list or vector containts cutoffs-value for variables, 
 #' this vector has the same length as .vars
 #'
 #' @return deprivation data.frame
@@ -28,11 +28,31 @@
 #'   z = 3
 #' )
 #' 
+#'   
+#' df %>% 
+#'   cutoffs_modify_at(c(x), cutoffs = c(x = 13))
+#' 
+#' df %>% 
+#'   cutoffs_modify_at(x, cutoffs = c(x = 13))
+#' 
 #' df %>%
-#'   cutoffs_modify_at(.vars = c("x", "y", "z"), cutoffs = cutoffs)
+#'   cutoffs_modify_at(.vars = c(x, y), cutoffs = cutoffs)
+#' 
+#' df %>%
+#'   cutoffs_modify_at(.vars = c(x, y, z), cutoffs = cutoffs)
+
+
 
 cutoffs_modify_at <- function(df, .vars, cutoffs) {
-  vars <- rlang::syms(.vars)
+  
+  vars <- as.list(rlang::quo_squash(rlang::enquo(.vars)))
+  
+  vars <- if (length(vars) == 1) {
+    vars
+  } else {
+    vars[-1]
+  }
+  
   quos <- purrr::map(vars, function(var) {
     rlang::quo(dplyr::if_else(!!var < cutoffs[[rlang::as_name(var)]], 1, 0))
   }) %>%
