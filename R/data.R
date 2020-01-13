f6da67c94170b451d805edb40afdb7c66c58c8af
perlatex <wangmj@sicnu.edu.cn>
2020-01-13 19:14:57 +0800

    add document

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Data from student test scores of different schools and classes in chengdu city
+#'
+#' This is data from the two test results from different schools in chengdu city
+#' 
+#'
+#' @format A data frame with 23868 rows and 5 variables:
+#' \describe{
+#'   \item{school}{school name}
+#'   \item{id}{school id}
+#'   \item{class}{class id}
+#'   \item{score_pre}{first test result}
+#'   \item{score_after}{second test result}
+#' }
+"chengdu"
