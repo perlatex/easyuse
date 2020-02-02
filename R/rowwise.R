c187cd837f1a866f0d9e675d625289aebf5e80b4
perlatex <wangmj@sicnu.edu.cn>
2020-02-02 22:41:59 +0800

    add_weighted_sum

--- R/rowwise.R
+++ R/rowwise.R
@@ -52,6 +52,59 @@ add_row_means <- function(.data, ..., .name = "row_mean", na.rm = TRUE) {
 
 
 
+#' Calculate the rowwise weighted sum for columns selected in data.frame
+#'
+#' @param .data data.frame
+#' @param ...  columns selected for rowwise `sum` or `mean` using `tidyselect` grammar
+#' @param .weights weights corresponding columns selected in dataframe
+#' @param .name  the name of new column
+#' @param na.rm whether to delete the missing value, `TRUE` or `FALSE`
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' df <- tribble(
+#'   ~id, ~x, ~y, ~z, ~g,
+#'   #--|--|--|--|--
+#'   "a", 13.1, 14, 4, 1,
+#'   "b", 11.2, 7, 5, 0,
+#'   "c", 12.5, 10, 1, 0,
+#'   "d", 20, 11, 3, 1
+#' )
+#'
+#'
+#' weights <- c(
+#'   x = 0.25,
+#'   y = 0.25,
+#'   z = 0.25,
+#'   g = 0.25
+#' )
+#'
+#' df %>% add_weighted_sum(x:g, .name = "wt_sum", .weights = weights)
+add_weighted_sum <- function(.data, ..., .weights, .name = "weighted_sum", na.rm = TRUE) {
+
+  sel <- tidyselect::vars_select(tbl_vars(.data), ...)
+  vars <- rlang::syms(sel)
+
+
+  quos <- purrr::map(vars, function(var) {
+    rlang::quo( !!var * .weights[[rlang::as_name(var)]] )
+  }) %>%
+    purrr::set_names(nm = purrr::map_chr(vars, rlang::as_name))
+
+
+  col <- .data %>%
+    dplyr::transmute(!!!quos)
+
+  .data %>%
+    dplyr::mutate(!!rlang::sym(.name) := purrr::pmap_dbl(col, sum, na.rm = na.rm))
+
+
+}
+
+
+
 #' Calculate the rowwise weighted mean for columns selected in data.frame
 #'
 #' @param .data data.frame
@@ -83,25 +136,26 @@ add_row_means <- function(.data, ..., .name = "row_mean", na.rm = TRUE) {
 #'
 #' df %>% add_weighted_mean(x:g, .name = "wt_mean", .weights = weights)
 add_weighted_mean <- function(.data, ..., .weights, .name = "weighted_mean", na.rm = TRUE) {
-
+  
   sel <- tidyselect::vars_select(tbl_vars(.data), ...)
   vars <- rlang::syms(sel)
-
-
+  
+  
   quos <- purrr::map(vars, function(var) {
     rlang::quo( !!var * .weights[[rlang::as_name(var)]] )
   }) %>%
     purrr::set_names(nm = purrr::map_chr(vars, rlang::as_name))
-
-
+  
+  
   col <- .data %>%
     dplyr::transmute(!!!quos)
-
+  
   .data %>%
-    dplyr::mutate(!!rlang::sym(.name) := purrr::pmap_dbl(col, sum, na.rm = na.rm))
-
-
+    dplyr::mutate(!!rlang::sym(.name) := purrr::pmap_dbl(col, lift_vd(mean), na.rm = na.rm))
+  
+  
 }
+
 
 
 
