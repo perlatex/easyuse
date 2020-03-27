11b1ac49efc9cfd15fffe2a30b3271230577b6f5
perlatex <wangmj@sicnu.edu.cn>
2020-03-27 13:21:07 +0800

    add chinese tones data

--- R/data.R
+++ R/data.R
@@ -12,3 +12,18 @@
 #'   \item{score_post}{second test result}
 #' }
 "chengdu"
+
+
+#' speech tones data
+#'
+#' speech tones data from experiment using praat, thank you very much for the data set provided by Wu xiaofang.
+#'
+#'
+#' @format A data frame with 16 rows and 14 variables:
+#' \describe{
+#'   \item{type}{tone type}
+#'   \item{word}{chinese word}
+#'   \item{p0-p100}{pitch serial}
+#'   \item{time}{speech time}
+#' }
+"ChineseTones"
--- data-raw/read_data.R
+++ data-raw/read_data.R
@@ -4,3 +4,8 @@
 chengdu <-
   readxl::read_excel('chengdu.xlsx')
 usethis::use_data(chengdu, overwrite = TRUE)
+
+
+ChineseTones <-
+	readr::read_csv('ChineseTones.csv')
+usethis::use_data(ChineseTones, overwrite = TRUE)
